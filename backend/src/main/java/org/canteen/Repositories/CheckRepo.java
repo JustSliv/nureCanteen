82a0c139674ead7cb841eceb65c51d275260b899
Michael <justslivus@gmail.com>
2020-12-02 14:16:49 +0300

    fix check

--- backend/src/main/java/org/canteen/Repositories/CheckRepo.java
+++ backend/src/main/java/org/canteen/Repositories/CheckRepo.java
@@ -11,8 +11,10 @@ import java.util.List;
 public interface CheckRepo  extends JpaRepository<Check, Long> {
 
    @Query(value = "SELECT checks.check_id, usr.email, checks.canteen, checks.date, product.name, basket.count, product.price\n" +
-      "FROM product, basket, usr, checks\n" +
-      "WHERE usr.id = basket.id_user AND basket.id_user = :user_id\n" +
-      "GROUP BY checks.check_id, usr.email, checks.canteen, checks.date, product.name, product.price, basket.count", nativeQuery = true)
+      "      FROM product, basket, usr, checks" +
+      "      WHERE usr.id = basket.id_user " +
+      "  AND basket.id_product = product.product_id" +
+      "  AND basket.id_user = :user_id" +
+      "      GROUP BY checks.check_id, usr.email, checks.canteen, checks.date, product.name, product.price, basket.count", nativeQuery = true)
    List<CheckDTO> getProductSumInCheck (@Param("user_id") Long user_id);
 }
