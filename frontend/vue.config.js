d0fa7c239075b8cd458bd96b1f2ee645a9475d73
Vladyslav <awesomer228@gmail.com>
2020-12-21 15:01:50 +0200

    add config for webpack

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,4 +1,23 @@
 module.exports = {
+  configureWebpack: (config) => {
+    if (process.env.NODE_ENV === 'production') {
+
+      config.optimization.minimizer[0].options.terserOptions = Object.assign(
+        {},
+        config.optimization.minimizer[0].options.terserOptions,
+        {
+          ecma: 5,
+          compress: {
+            keep_fnames: true,
+          },
+          warnings: false,
+          mangle: {
+            keep_fnames: true,
+          },
+        },
+      );
+    }
+  },
   "transpileDependencies": [
     "vuetify"
   ]
